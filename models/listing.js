1b41034b3059b715a1eead995fb0729b13f1dd8e
MRUNAL-BAVISKAR <ashishbaviskar724@gmail.com>
2024-06-01 15:28:46 +0530

    Updated Version

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 const listingSchema = new Schema({
     title: {type: String,
     required: true,},
@@ -15,7 +16,15 @@ const listingSchema = new Schema({
     country: String,
     reviews:[{
         type: Schema.Types.ObjectId,
+        ref:"Review"
     }]
 });
+
+listingSchema.post("findOneAndDelete", async(listing)=>{
+    if(listing){
+        await Review.deleteMany({_id:{$in: listing.reviews}});
+    }
+});
+
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
