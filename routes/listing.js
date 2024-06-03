29c488b9d486d14aff040d1054dde6de475c6619
MRUNAL-BAVISKAR <ashishbaviskar724@gmail.com>
2024-06-03 13:28:12 +0530

    Updated files

---
+++ routes/listing.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("../utils/wrapAsync.js");
+const ExpressError = require("../utils/ExpressError.js");
+const { listingSchema, reviewSchema } = require("../schema.js");
+const Listing = require("../models/listing.js");
+
+
+//Validate Listing function 
+const validateListing = (req, res, next) =>{
+    let {error} = listingSchema.validate(req.body);
+    
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
+};
+
+// Index Route
+router.get("/", wrapAsync(async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+}));
+
+//New Route 
+router.get("/new", (req, res) => {
+    res.render("listings/new.ejs");
+});
+
+
+//Show Route 
+router.get("/:id", wrapAsync(async (req, res) => {
+    try {
+        let { id } = req.params;
+        const listing = await Listing.findById(id).populate("reviews");
+        if (!listing) {
+            return res.status(404).send("Listing not found");
+        }
+        res.render("listings/show", { listing });
+    } catch (err) {
+        res.status(500).send("Server error");
+    }
+}));
+
+//Create Route 
+router .post("/",  validateListing, wrapAsync(async (req, res, next) => {
+    let result = listingSchema.validate(req.body);
+    console.log(result);
+    if (result.error) {
+        throw new ExpressError(400, result.error);
+    }
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+}));
+
+
+//Edit Route
+router.get("/:id/edit", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+}));
+
+//Update Route
+router.put("/:id",  validateListing, wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+}));
+
+
+//Delete Route
+router.delete("/:id", wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    let deleteListing = await Listing.findByIdAndDelete(id);
+    console.log(deleteListing);
+    res.redirect("/listings");
+}));
+module.exports = router;
---
+++ routes/review.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { reviewSchema } = require("../schema.js");
+const Review = require("../models/review.js");
+const Listing = require("../models/listing.js");
+
+
+//Validate Review Function
+const validateReview= (req, res, next) =>{
+    let {error} = reviewSchema.validate(req.body);
+    
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
+}; 
+
+//Reviews 
+//Post Route 
+router.post("/", validateReview,wrapAsync(async (req, res) => {
+    try {
+        let listing = await Listing.findById(req.params.id);
+        if (!listing) {
+            return res.status(404).send("Listing not found");
+        }
+        let newReview = new Review(req.body.review);
+        listing.reviews.push(newReview);
+        await newReview.save();
+        await listing.save();
+        console.log("New Review Saved");
+        res.redirect(`/listings/${listing._id}`);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send("Internal Server Error");
+    }
+   
+}));
+
+// Delete Review Route 
+router.delete("/:reviewId", wrapAsync(async(req,res)=>{
+    let{id, reviewId} = req.params;
+    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
+    await Review.findByIdAndUpdate(reviewId);
+    res.redirect(`/listings/${id}`);
+}));
+
+module.exports = router;
