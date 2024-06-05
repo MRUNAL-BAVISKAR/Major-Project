0bdc7612f8bd52eca3bc3fcab8206306ddffce7d
MRUNAL-BAVISKAR <ashishbaviskar724@gmail.com>
2024-06-05 15:22:26 +0530

    Updated Version

--- middleware.js
+++ middleware.js
@@ -6,3 +6,9 @@ module.exports.isLoggedIn = (req,res,next) =>{
     }
     next();
 }
+module.exports.saveRedirectUrl = (req, res, next)=>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+};
--- routes/user.js
+++ routes/user.js
@@ -8,47 +8,41 @@ router.get("/signup", (req, res)=>{
     res.render("users/signup.ejs");
 });
 
-
-router.post("/signup",wrapAsync( async(req,res)=>{
-    try{
-        let{username, email, password} = req.body;
-        const newUser = new User({ email, username});
+router.post("/signup", wrapAsync(async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({ email, username });
         const registerUser = await User.register(newUser, password);
         console.log(registerUser);
         req.flash("success", "Welcome to WanderLust!");
-        res.redirect("/listings");
-
-        req.login(registerUser, (err)=>{
-             if(err){
+        req.login(registerUser, (err) => {
+            if (err) {
                 return next(err);
-             }
-             req.flash("success", "Welcome to WanderLust!");
-             res.redirect("/listings");
+            }
+            req.flash("success", "Welcome to WanderLust!");
+            res.redirect("/listings");
         });
-    }catch(e){
+    } catch (e) {
         req.flash("error", e.message);
         res.redirect("/signup");
     }
-  
 }));
 
 router.get("/login", (req,res)=>{
     res.render("users/login.ejs");
 });
 
-
-router.post("/login",saveRedirectUrl,
-passport.authenticate("local", 
-{failureRedirect:'/login',
- failureFlash: true}),
- async(req,res)=>{
-    res.flash("success","Welcome to WanderLust! You are logged in!");
-
-    res.redirect("/listings");
-
-    let redirectUrl = req.locals.redirectUrl || "/listings";
-    res.redirect(redirectUrl);
-});
+router.post("/login", saveRedirectUrl,
+    passport.authenticate("local",
+        {
+            failureRedirect: '/login',
+            failureFlash: true
+        }),
+    async (req, res) => {
+        req.flash("success", "Welcome to WanderLust! You are logged in!");
+        let redirectUrl = res.locals.redirectUrl || "/listings";
+        res.redirect(redirectUrl);
+    });
 
 router.get("/logout", (req,res, next)=>{
     req.logout((err)=>{
